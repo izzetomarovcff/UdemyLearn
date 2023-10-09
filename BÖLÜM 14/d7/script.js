04f525cbb42e7a4b816d4e1a62aca2d27c8a8a5c
izzetomarovcff <izzetomarovcff@gmail.com>
2023-10-09 20:34:06 +0400

    b14d7

---
+++ BÖLÜM 14/d7/script.js
@@ -0,0 +1,9 @@
+function prntCount(){
+    var word = prompt("Please enter word");
+    var count = prompt("Please enter count");
+    for( let i =0 ; i<count; i++){
+        console.log(word)
+    }
+}
+
+prntCount()
