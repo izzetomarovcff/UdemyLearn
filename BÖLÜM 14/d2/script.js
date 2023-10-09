4e3da352a03101085a94b3dd1e23b97247bdf443
izzetomarovcff <izzetomarovcff@gmail.com>
2023-10-09 12:44:55 +0400

    b14d2

---
+++ BÖLÜM 14/d2/script.js
@@ -0,0 +1,17 @@
+let url = "https://www.sadikturan.com/";
+let kursAdi = "Komple Web Geliştirme Kursu";
+
+// #1
+// console.log(url.length) 
+
+// #2
+// console.log(kursAdi.split(" ").length)
+
+// #3
+// console.log(url.indexOf("https") == 0 ? "Evet":"Hayir")
+
+// #4
+// console.log(kursAdi.indexOf("Kursu") != -1 ? "Var" : "Yoktur")
+
+// #5
+// console.log(`${url}${kursAdi.toLowerCase().replace("ş", "s").replaceAll(" ", "-")}`)
