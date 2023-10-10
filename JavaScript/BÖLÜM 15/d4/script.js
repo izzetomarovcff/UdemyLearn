68eaadf4bbe4e447ebc4ad3dab4215c347cbbd8a
izzetomarovcff <izzetomarovcff@gmail.com>
2023-10-10 14:12:37 +0400

    b14d4

---
+++ JavaScript/BÖLÜM 15/d4/script.js
@@ -0,0 +1,23 @@
+let result;
+
+let gorevListesi = [
+    {"id": 1, gorevAdi: "Gorev 1"},
+    {"id": 2, gorevAdi: "Gorev 2"},
+    {"id": 3, gorevAdi: "Gorev 3"},
+    {"id": 4, gorevAdi: "Gorev 4"},
+];
+ul =  document.getElementById("task-list")
+
+
+for(let gorev of gorevListesi){
+    let li = `
+    <li class="task list-group-item">
+        <div class="form-check">
+            <input type="checkbox" id="${gorev.id}" class="form-check-input">
+            <label for="${gorev.id}" class="form-check-label">${gorev.gorevAdi}</label>
+        </div>
+    </li>  
+`;
+ul.insertAdjacentHTML("beforeend", li)
+}
+console.log(result)
