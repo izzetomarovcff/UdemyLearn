f9f1c2b8ded179edffe7784d5797b6f356c8cd22
izzetomarovcff <izzetomarovcff@gmail.com>
2023-10-11 14:29:07 +0400

    b16d4

---
+++ JavaScript/BÖLÜM 16/d4/script.js
@@ -0,0 +1,42 @@
+
+function Soru(soruMetni, cevapSecenekleri,dogruCevap){
+    this.soruMetni = soruMetni
+    this.cevapSecenekleri = cevapSecenekleri
+    this.dogruCevap = dogruCevap
+}
+
+Soru.prototype.cevabiKontrolEt = function (cevap){
+    return cevap === this.dogruCevap
+}
+
+let soru1 = new Soru("Hangisi JavaScript paket yonetim uygulamasidir?",{a: "node.js",b: "TypeScript",c: "Npm"}, "c")
+let soru2 = new Soru("Hangisi .Net paket yonetim uygulamasidir?",{a: "node.js",b: "nuget",c: "Npm"}, "b")
+
+let sorular = [
+    new Soru("1 Hangisi JavaScript paket yonetim uygulamasidir?",{a: "node.js",b: "TypeScript",c: "Npm"}, "c"),
+    new Soru("2 Hangisi JavaScript yonetim uygulamasidir?",{a: "node.js",b: "TypeScript",c: "Npm"}, "c"),
+    new Soru("3 Hangisi JavaScript paket yonetim uygulamasidir?",{a: "node.js",b: "TypeScript",c: "Npm"}, "c"),
+    new Soru("4 Hangisi JavaScript paket yonetim uygulamasidir?",{a: "node.js",b: "TypeScript",c: "Npm"}, "c")
+]
+
+function Quiz(sorular){
+    this.sorular = sorular
+    this.soruIndex = 0
+}
+
+Quiz.prototype.soruGetir = function(){
+   return this.sorular[this.soruIndex]
+}
+
+const quiz = new Quiz(sorular)
+
+
+document.querySelector(".btn-start").addEventListener("click", function(){
+    if(quiz.sorular.length != quiz.soruIndex){
+        console.log(quiz.soruGetir())
+        quiz.soruIndex++
+    }else{
+        console.log("quiz bitti")
+    }
+    
+})
