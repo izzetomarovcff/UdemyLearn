a0974cb9631dc5f02f218d24cd93dab2bfa370d1
izzetomarovcff <izzetomarovcff@gmail.com>
2023-10-18 12:13:21 +0400

    b17d1

---
+++ JavaScript/BÖLÜM 17/d1/script.js
@@ -0,0 +1,17 @@
+// ES5 v1
+function selamlamaES5v1(){
+    console.log("Merhaba selamlamaES5v1")
+}
+selamlamaES5v1()
+
+// ES5 v2
+var selamlamaES5v2 = function(){
+    console.log("Merhaba selamlamaES5v2");
+}
+selamlamaES5v2()
+
+// ES6 arrow function
+const selamlamaES6 = ()=>{
+    console.log("Merhaba selamlamaES6");
+}
+selamlamaES6()
