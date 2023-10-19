151070507d8275c78088dfd19fa07c38b85444f7
izzetomarovcff <izzetomarovcff@gmail.com>
2023-10-20 01:03:05 +0400

    music player end

--- JavaScript/BÖLÜM 17/music-player/musicplayer.js
+++ JavaScript/BÖLÜM 17/music-player/musicplayer.js
@@ -1,25 +1,27 @@
-class MusicPlayer{
-    constructor(musicList){
+class MusicPlayer {
+    constructor(musicList) {
         this.musicList = musicList;
-        this.index = 1
+        this.index = 1;
     }
-    getMusic(){
-        return this.musicList[this.index]
+
+    getMusic() {
+        return this.musicList[this.index];
     }
-    next(){
-        if(this.index + 1 != this.musicList.length){
-            this.index++
+
+    next() {
+        if(this.index + 1 < this.musicList.length) {
+            this.index++;
         }
-        else{
-            this.index = 0
+        else {
+             this.index = 0;
         }
     }
-    prev(){
-        if(this.index != 0){
-            this.index--
-        }
-        else{
-            this.index =this.musicList.length - 1
+
+    prev() {
+        if(this.index != 0) {
+            this.index--;
+        } else {
+            this.index = this.musicList.length - 1;
         }
     }
 }
