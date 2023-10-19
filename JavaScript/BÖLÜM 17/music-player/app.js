fac80e7e8c7b8a0f45cb98add69ed0fec03468b3
izzetomarovcff <izzetomarovcff@gmail.com>
2023-10-19 15:28:23 +0400

    music player get details

---
+++ JavaScript/BÖLÜM 17/music-player/app.js
@@ -0,0 +1,34 @@
+const player = new MusicPlayer(musicList)
+
+
+let music = player.getMusic();
+console.log(music.getName())
+
+player.next()
+music = player.getMusic();
+console.log(music.getName())
+
+
+player.next()
+music = player.getMusic();
+console.log(music.getName())
+
+player.next()
+music = player.getMusic();
+console.log(music.getName())
+
+player.next()
+music = player.getMusic();
+console.log(music.getName())
+
+player.next()
+music = player.getMusic();
+console.log(music.getName())
+
+player.next()
+music = player.getMusic();
+console.log(music.getName())
+
+player.previous()
+music = player.getMusic();
+console.log(music.getName())
