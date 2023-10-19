c18c18aebbafbbeb4480b5cec39bb0e2bb7bf3bf
izzetomarovcff <izzetomarovcff@gmail.com>
2023-10-20 00:25:47 +0400

    music player sound control

--- JavaScript/BÖLÜM 17/music-player/app.js
+++ JavaScript/BÖLÜM 17/music-player/app.js
@@ -8,6 +8,8 @@ const next = document.querySelector("#controls #next")
 const duration = document.querySelector("#duration")
 const currentTime = document.querySelector("#current-time")
 const progressBar = document.querySelector("#progress-bar")
+const volume = document.querySelector("#volume")
+const volumeBar = document.querySelector("#volume-bar")
 const player = new MusicPlayer(musicList)
 
 
@@ -79,4 +81,33 @@ audio.addEventListener("timeupdate",()=>{
 progressBar.addEventListener("input",()=>{
     currentTime.textContent = calculateTime(progressBar.value)
     audio.currentTime = progressBar.value
+})
+
+let sesDurumu = "sesli";
+
+volumeBar.addEventListener("input",(e)=>{
+    const value = e.target.value
+    audio.volume = value / 100
+    if(value == 0){
+        audio.muted = true
+        sesDurumu = "sessiz"
+        volume.classList = "fa-solid fa-volume-xmark"
+    }else{
+        audio.muted = false
+        sesDurumu = "sesli"
+        volume.classList = "fa-solid fa-volume-high"
+    }
+})
+volume.addEventListener("click",()=>{
+    if(sesDurumu === "sesli"){
+        audio.muted = true
+        sesDurumu = "sessiz"
+        volume.classList = "fa-solid fa-volume-xmark"
+        volumeBar.value = 0
+    }else{
+        audio.muted = false
+        sesDurumu = "sesli"
+        volume.classList = "fa-solid fa-volume-high"
+        volumeBar.value = 100
+    }
 })
