c7543fa4af111b922e57431320fcb69fd5c40624
izzetomarovcff <izzetomarovcff@gmail.com>
2023-10-23 00:14:07 +0400

    list

--- ANGULAR/ng-app/src/app/product-list/product-list.component.ts
+++ ANGULAR/ng-app/src/app/product-list/product-list.component.ts
@@ -21,4 +21,32 @@ export class ProductListComponent implements OnInit {
     isActive: true
   }
 
+  productsList = ["iphone 14","iphone 15","iphone 16"]
+  products = [
+    {
+      id: 1,
+      name: "iphone 14",
+      price: 20000,
+      imageUrl:"1.jpeg",
+      description: "Iyi Telefon",
+      isActive: true
+    },
+    {
+      id: 2,
+      name: "iphone 15",
+      price: 30000,
+      imageUrl:"2.jpeg",
+      description: "Iyi Telefon",
+      isActive: false
+    },
+    {
+      id: 3,
+      name: "iphone 16",
+      price: 40000,
+      imageUrl:"3.jpeg",
+      description: "Iyi Telefon",
+      isActive: true
+    }
+  ]
+
 }
