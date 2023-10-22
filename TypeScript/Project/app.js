a7a5886af17f2539f229c56a1c2ded780c520d5b
izzetomarovcff <izzetomarovcff@gmail.com>
2023-10-22 21:38:41 +0400

    PRJ

---
+++ TypeScript/Project/app.js
@@ -0,0 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var ProductService_1 = require("./ProductService");
+var product_1 = require("./product");
+var _productService = new ProductService_1.ProductService();
+var result;
+result = _productService.getProducts();
+result = _productService.getById(2);
+var p = new product_1.Product();
+p.id = 2;
+p.name = "Iphone 6";
+p.price = 4000;
+p.category = "Telefon";
+_productService.saveProduct(p);
+// _productService.deleteProduct(result)
+result = _productService.getProducts();
+console.log(result);
